c88a9edffbd872a3b0d5a52edf6b94bf67f9ffd5
vxibh <vxx6hxv@gmail.com>
2024-03-11 15:03:50 +0530

    pageAction modified

--- src/actions/pageActions.js
+++ src/actions/pageActions.js
@@ -1,21 +1,72 @@
 'use server';
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { Page } from "@/models/Page";
-import mongoose from 'mongoose';
+import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+import {Page} from "@/models/Page";
+import {User} from "@/models/User";
+import mongoose from "mongoose";
+import {getServerSession} from "next-auth";
 
+export async function savePageSettings(formData) {
+  mongoose.connect(process.env.MONGODB_URI);
+  const session = await getServerSession(authOptions);
+  if (session) {
+    const dataKeys = [
+      'displayName','location',
+      'bio', 'bgType', 'bgColor', 'bgImage',
+    ];
 
-export async function savePageSettings(formData){
-    mongoose.connect(process.env.MONGODB_URI);
-    const session = await getServerSession(authOptions);
-    if(session) {
-        const displayName = formData.get('displayName');
-        const location = formData.get('location');
-        const bio = formData.get('bio');
-        await Page.updateOne({ owner: session?.user?.email }, {
-            displayName, location, bio
-        });
-        return true;
+    const dataToUpdate = {};
+    for (const key of dataKeys) {
+      if (formData.has(key)) {
+        dataToUpdate[key] = formData.get(key);
+      }
     }
+
+    await Page.updateOne(
+      {owner:session?.user?.email},
+      dataToUpdate,
+    );
+
+    if (formData.has('avatar')) {
+      const avatarLink = formData.get('avatar');
+      await User.updateOne(
+        {email: session.user?.email},
+        {image: avatarLink},
+      );
+    }
+
+    return true;
+  }
+
+  return false;
+}
+
+export async function savePageButtons(formData) {
+  mongoose.connect(process.env.MONGODB_URI);
+  const session = await getServerSession(authOptions);
+  if (session) {
+    const buttonsValues = {};
+    formData.forEach((value, key) => {
+      buttonsValues[key] = value;
+    });
+    const dataToUpdate = {buttons:buttonsValues};
+    await Page.updateOne(
+      {owner:session?.user?.email},
+      dataToUpdate,
+    );
+    return true;
+  }
+  return false;
+}
+
+export async function savePageLinks(links) {
+  mongoose.connect(process.env.MONGODB_URI);
+  const session = await getServerSession(authOptions);
+  if (session) {
+    await Page.updateOne(
+      {owner:session?.user?.email},
+      {links},
+    );
+  } else {
     return false;
+  }
 }
