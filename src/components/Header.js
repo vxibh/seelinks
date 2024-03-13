c81141e19d8e5a91dbb5ba91e09831127df4fe1d
vxibh <vxx6hxv@gmail.com>
2024-03-13 18:54:40 +0530

    header modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,44 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { getServerSession } from "next-auth";
+
+import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+import LogoutButton from "@/components/buttons/LogoutButton";
+import {faLink} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {getServerSession} from "next-auth";
 import Link from "next/link";
-import LogoutButton from "./buttons/LogoutButton";
-import { faLink } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default async function Header() {
-
-    const session = await getServerSession(authOptions);
-    return (
-        <header className="bg-white border-b py-4">
-        <div className="max-w-4xl flex justify-between mx-auto px-6">
-            <div className="flex items-center gap-6 ">
-                <Link href={"/"} className="flex items-center gap-2 text-blue-700 ">
-                <FontAwesomeIcon icon={faLink} className="text-blue-700 font-bold"/>
-                    <span className="font-bold">Seelinks</span></Link>
-                <nav className="flex items-center gap-4 text-slate-500 text-sm">
-                    <Link href={"/about"}>About</Link>
-                    <Link href={"/pricing"}>Pricing</Link>
-                    <Link href={"/contact"}>Contact</Link>
-                </nav>
-            </div>
-            <div className="flex gap-6">
-                <nav className="flex items-center gap-4 text-slate-500 text-sm">
-                {!!session && (
-                    <>
-                        <Link href={"/dashboard"}>
-                            Dashboard
-                        </Link>
-                        <LogoutButton />
-                    </>
-                )}
-                {!session && (
-                    <>
-                        <Link href={"/login"}>Login</Link>
-                        <Link href={"/signup"}>Signup</Link>
-                    </>
-                )}
-                </nav> 
-            </div>
+  const session = await getServerSession(authOptions);
+  return (
+    <header className="bg-white border-b py-4">
+      <div className="max-w-4xl flex justify-between mx-auto px-6">
+        <div className="flex items-center gap-6">
+          <Link href={'/'} className="flex items-center gap-2 text-blue-500">
+            <FontAwesomeIcon icon={faLink} className="text-blue-500" />
+            <span className="font-bold">Seelinks</span>
+          </Link>
+          <nav className="flex items-center gap-4 text-slate-500 text-sm">
+            <Link href={'/about'}>About</Link>
+            <Link href={'/pricing'}>Pricing</Link>
+            <Link href={'/contact'}>Contact</Link>
+          </nav>
         </div>
-        </header>
-    );
+        <nav className="flex items-center gap-4 text-sm text-slate-500">
+          {!!session && (
+            <>
+              <Link href={'/account'}>
+                Hello, {session?.user?.name}
+              </Link>
+              <LogoutButton />
+            </>
+          )}
+          {!session && (
+            <>
+              <Link href={'/login'}>Sign In</Link>
+              <Link href={'/login'}>Create Account</Link>
+            </>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
 }
